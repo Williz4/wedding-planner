49c402b759dfd681e81fe2b40ec6fd1527aeb69e
david mark <markkariuki17@gmail.com>
2017-11-09 15:00:51 +0300

    add changes

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-		$("form #submit").click(function() {
-
-  	var email= $(".Email").val();
-    alert(email+" "+"has been successfully added to our list of subscribers.Thank you");
-  });
-});
