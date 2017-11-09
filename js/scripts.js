4969acbdd4c7871563eec25519b6ac1c1bc51cc8
Brenda Okumu <brenda.okumu2@gmail.com>
2017-11-09 10:58:55 +0300

    Add lightbox to wedding cakes section

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,53 @@ $(document).ready(function() {
   	var email= $(".Email").val();
     alert(email+" "+"has been successfully added to our list of subscribers.Thank you");
   });
+
+
+	$( ".displayed" ).hover(
+	  function() {
+	    $( ".displayed span" ).toggleClass("hidden");
+  });
+
+	$( "img.displayed" ).hover(function() {
+	  $( this ).fadeOut( 100 );
+	  $( this ).fadeIn( 500 );
+	});
+
+
+
+	$( ".displayed2" ).hover(
+		function() {
+			$( ".displayed2 span" ).toggleClass("hidden");
+	});
+
+	$( "img.displayed2" ).hover(function() {
+		$( this ).fadeOut( 100 );
+		$( this ).fadeIn( 500 );
+	});
+
+
+		// Four images side by side
+		document.querySelector('.btn-cake6').addEventListener('click', function() {
+		 var columns = document.getElementsByClassName('columning');
+		 for (i = 0; i < columns.length; i++) {
+			columns[i].style.width = "21%";
+				}
+			});
+
+		// Two images side by side
+		document.querySelector('.btn-cake5').addEventListener('click', function() {
+		var columns = document.getElementsByClassName('columning');
+		for (i = 0; i < columns.length; i++) {
+		 columns[i].style.width = "20.5%";
+			 }
+		 });
+
+		 // Full-width images
+		 document.querySelector('.btn-cake4').addEventListener('click', function() {
+		 var columns = document.getElementsByClassName('columning');
+		 for (i = 0; i < columns.length; i++) {
+			columns[i].style.width = "20%";
+				}
+			});
+
 });
