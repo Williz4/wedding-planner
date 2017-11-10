5763205157033359c218ada6f5fe7ed527a9145c
Brenda Okumu <brenda.okumu2@gmail.com>
2017-11-10 04:36:30 +0300

    Modify catering section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-		$("form #submit").click(function() {
-
-  	var email= $(".Email").val();
-    alert(email+" "+"has been successfully added to our list of subscribers.Thank you");
-  });
-
-
 	$( ".displayed" ).hover(
 	  function() {
 	    $( ".displayed span" ).toggleClass("hidden");
